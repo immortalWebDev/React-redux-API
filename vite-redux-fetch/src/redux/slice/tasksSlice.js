8885bea7567d56761bb9a2006183bc94cc936b86
immortalWebDev <pgbadgujar007@gmail.com>
2024-12-07 18:33:20 +0530

    Used axios insted of fetch for all crud operations

--- vite-redux-fetch/src/redux/slice/tasksSlice.js
+++ vite-redux-fetch/src/redux/slice/tasksSlice.js
@@ -1,172 +1,133 @@
-import { createSlice, createAsyncThunk} from "@reduxjs/toolkit";
-
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const API_URL = import.meta.env.VITE_MOCKAPI_URL;
 
 export const fetchTasksAsync = createAsyncThunk(
   "tasks/fetchTasksAsync",
-  async (_,{rejectWithValue}) => { //pass {rejectWithValue} as second arg always
+  async (_, { rejectWithValue }) => {
+    //pass {rejectWithValue} as second arg always
     //try catch block is recommended for all async tasks
     try {
       //Making fetch request (GET)
-      const response = await fetch(API_URL); //GET is default method if not specified
+      const response = await axios.get(API_URL); //GET is default method if not specified axios('url) works
 
-      //If status code is outside 200-299
-      if (!response.ok) {
-        // Throw error with detailed message including status
-        throw new Error(
-          `Failed to fetch data: ${response.statusText} , Status code: ${response.status}`
-        );
-      }
-
-      //NOTE: //if wanna handle 404 or 500 explicitly , you can write separate logic for that
-
-      // Parsing JSON data from response (successfull)
-      const data = await response.json();
-      console.log("from GET: ", data);
+      const data = await response.data;
+      console.log("from axios GET: ", data);
 
       //Returning data to be used by reducers
       return data;
     } catch (err) {
-      console.error("Error during fetch operation", err);
+      console.error("Error during axios get operation", err.message);
       // throw err; // Re-throw the error if you want it to be caught in a higher-level handler
 
       // Optionally, you can return a default value to prevent the app from crashing
       // return []; // Example of returning an empty array as fallback
 
-      return rejectWithValue("Sorry, can't fetch right now. Try again later!") //shows on display for user
+      // return rejectWithValue("Sorry, can't fetch right now. Try again later!") //shows on display for user
 
-      /*
-    If fetch call directly fails, and we do not get response object then the flow will come to catch block straight
-    Suppose the url is wrong then we do not even get response object
-    at that very moment try block execution stops and the flow is forwarded to catch block
-    to handle the error gracefully, so that the app doesnt crash
-    
-    But if the call is succefull but we get status outside of 200 range, then we need
-    to handle it explicitly if we want, otherwise the flow will go to catch block
-    */
+      //with axios we can display many responses to user
+      if (err.response) {
+        return rejectWithValue(
+          `Failed to fetch tasks. Server responded with: ${err.response.statusText} (Status: ${err.response.status})`
+        );
+      } else if (err.request) {
+        // Request was made but no response received
+        return rejectWithValue(
+          "No response from the server. Please try again later."
+        );
+      } else {
+        // Something else happened while setting up the request
+        return rejectWithValue(`Error: ${err.message}`);
+      }
     }
   }
 );
 
 export const addTaskAsync = createAsyncThunk(
   "tasks/addTaskAsync",
-  async (newTask,{rejectWithValue}) => {
-try{
+  async (newTask, { rejectWithValue }) => {
+    try {
+      const response = await axios.post(API_URL, newTask);
 
-    const response = await fetch(API_URL, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" }, //not case-sensitive but important for fetch() 
-      body: JSON.stringify(newTask),
-    });
-    
-    // Check if the response is OK (2xx range)
-    if (!response.ok) {
-      // Throw an error with the status code and message for debugging
-      throw new Error(`Failed to add blog task: ${response.statusText}, Status code: ${response.status}`);
+      // Parse the JSON response from the server
+      const data = await response.data;
+      console.log("from axios POST: ", response);
+
+      return data; //this has additional id property comes from server
+    } catch (err) {
+      console.error("Error while adding new blog:", err.message);
+
+      // throw err //throw err to stop app from crashing
+
+      //Here also we can handle the various errors just like above axios.get() catch block
+
+      return rejectWithValue("Sorry, Failed to add a new blog");
     }
-
-    // Parse the JSON response from the server
-    const data = await response.json();
-    console.log("from POST: ", data);
-
-    return data; //this has additional id property comes from server
-  }catch(err){
-
-    console.error("Error while adding new blog:", err.message);
-
-    // throw err //throw err to stop app from crashing
-
-    return rejectWithValue("Sorry, Failed to add a new blog") 
-
-
-  }
   }
 );
 
 export const updateTaskAsync = createAsyncThunk(
   "tasks/updateTaskAsync",
-  async ({ id, updatedTask },{rejectWithValue}) => {
+  async ({ id, updatedTask }, { rejectWithValue }) => {
+    try {
+      const response = await axios.put(`${API_URL}/${id}`, updatedTask);
 
-    try{
+      const data = await response.data;
 
-    
-    const response = await fetch(`${API_URL}/${id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(updatedTask),
-    });
-    if (!response.ok) {
-      throw new Error(
-        `Failed to update blog: ${response.statusText}, Status code: ${response.status}`
-      );
+      console.log("from axios PUT: ", data);
+
+      return data;
+    } catch (err) {
+      console.error("Error in PUT request:", err.message);
+
+      //Here also can display various errors
+      return rejectWithValue("Sorry, Failed to update the blog!");
     }
-
-    const data = await response.json();
-    console.log("from PUT: ", data);
-    return data;
-  }catch(err){
-
-    console.error("Error in PUT request:", err.message);
-    
-    return rejectWithValue('Sorry, Failed to update the blog!')
-
-  }
   }
 );
 
 export const deleteTaskAsync = createAsyncThunk(
   "tasks/deleteTaskAsync",
-  async (id,{rejectWithValue}) => {
-    try{
+  async (id, { rejectWithValue }) => {
+    try {
+      const response = await axios.delete(`${API_URL}/${id}`);
+      
+      
+      //just to check 
+      if(response.statusText === "OK" && response.status === 200)
+      {
+        console.log('Deleted successfully')
+      }
 
-    
-    const response = await fetch(`${API_URL}/${id}`, {
-      method: "DELETE",
-    });
-    if (!response.ok) {
-      throw new Error(
-        `Failed to update blog: ${response.statusText}, Status code: ${response.status}`
-      );
+      console.log("from axios DELETE id number: ", id);
+
+      return id; //typically return id only, as its used in reducer to updtate the state
+    } catch (err) {
+      console.log("Error while deleting: ", err.message);
+
+      //can use various errors
+      return rejectWithValue("Failed to delete, Try again later");
     }
-
-    console.log("from DELETE: ", id);
-
-    return id;
-  }catch(err){
-    console.log("Error while deleting: ", err.message)
-
-    return rejectWithValue("Failed to delete, Try again later")
-  }
   }
 );
 
 export const updateReadStatusAsync = createAsyncThunk(
   "tasks/updateReadStatusAsync",
-  async ({ id, updatedTask },{rejectWithValue}) => {
-    try{
+  async ({ id, updatedTask }, { rejectWithValue }) => {
+    try {
+      const response = await axios.put(`${API_URL}/${id}`, updatedTask);
 
-    
-    const response = await fetch(`${API_URL}/${id}`, {
-      method: "PUT",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(updatedTask),
-    });
+      const data = await response.data;
+      console.log("Updated reading status by axios: ", data);
 
-    if (!response.ok) {
-      throw new Error(
-        `Failed to update blog: ${response.statusText}, Status code: ${response.status}`
-      );
+      return data; //sent to reducer
+
+    } catch (err) {
+      console.log("Failed to update reading status: ", err.message);
+
+      return rejectWithValue("Failed to update reading status, retry later");
     }
-    const data = await response.json();
-    console.log('Updated reading status: ', data)
-    return data
-
-  }catch(err){
-    console.log("Failed to update reading status: " ,err.message)
-
-    return rejectWithValue("Failed to update reading status, retry later")
-  }
   }
 );
 
@@ -188,7 +149,7 @@ const tasksSlice = createSlice({
         state.isLoading = false;
         state.taskList = action.payload;
       })
-      .addCase(fetchTasksAsync.rejected, (state,action) => {
+      .addCase(fetchTasksAsync.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = action.payload;
       });
@@ -202,7 +163,7 @@ const tasksSlice = createSlice({
         state.isLoading = false;
         state.taskList.push(action.payload);
       })
-      .addCase(addTaskAsync.rejected, (state,action) => {
+      .addCase(addTaskAsync.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = action.payload;
       });
@@ -221,7 +182,7 @@ const tasksSlice = createSlice({
           state.taskList[index] = action.payload;
         }
       })
-      .addCase(updateTaskAsync.rejected, (state,action) => {
+      .addCase(updateTaskAsync.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = action.payload;
       });
@@ -237,7 +198,7 @@ const tasksSlice = createSlice({
           (task) => task.id !== action.payload
         );
       })
-      .addCase(deleteTaskAsync.rejected, (state,action) => {
+      .addCase(deleteTaskAsync.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = action.payload;
       });
@@ -256,12 +217,11 @@ const tasksSlice = createSlice({
           state.taskList[index] = action.payload;
         }
       })
-      .addCase(updateReadStatusAsync.rejected, (state,action) => {
+      .addCase(updateReadStatusAsync.rejected, (state, action) => {
         state.isLoading = false;
         state.isError = action.payload;
       });
   },
 });
 
-// export const { setTasks, addTask, updateTask, deleteTask,setLoading,setError } = tasksSlice.actions;
 export default tasksSlice.reducer;
